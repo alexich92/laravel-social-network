a520d9eb096f6b44cdddb77e663ccd9a3c02b323
alex <alexich_92@yahoo.com>
2017-10-06 14:35:20 +0300

    Display user picture in the navbar

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -33,7 +33,7 @@
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
+                                <img src="{{asset('images/avatars/' . Auth::user()->avatar)}}" style="margin-left: -10px; margin-top: -6px ; padding-top: 1px"  height="32px" alt="">
                             </a>
 
                             <ul class="dropdown-menu" role="menu">
